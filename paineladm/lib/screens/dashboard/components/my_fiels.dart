6f90e148ac94f414d5c0242ff419201e040556ea
dandy <danielsbarbosa27@gmail.com>
2021-05-29 02:15:17 -0300

    MyFiles

---
+++ paineladm/lib/screens/dashboard/components/my_fiels.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+
+class MyFiels extends StatelessWidget {
+  const MyFiels({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "My Files",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            ElevatedButton.icon(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: defaultPadding * 1.5,
+                        vertical: defaultPadding)),
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text("Add New"))
+          ],
+        )
+      ],
+    );
+  }
+}
--- paineladm/lib/screens/dashboard/dashboard_screen.dart
+++ paineladm/lib/screens/dashboard/dashboard_screen.dart
@@ -3,10 +3,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:paineladm/constants.dart';
 
-import 'components/chart.dart';
 import 'components/header.dart';
+import 'components/my_fiels.dart';
 import 'components/storage_details.dart';
-import 'components/storage_info_card.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
@@ -25,19 +24,13 @@ class DashboardScreen extends StatelessWidget {
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                // ignore: prefer_const_literals_to_create_immutables
                 children: [
-                  Expanded(
-                      flex: 5,
-                      child: Container(
-                        height: 500,
-                        color: secondaryColor,
-                      )),
-                  const SizedBox(
+                  const Expanded(flex: 5, child:  MyFiels()),
+                  const  SizedBox(
                     width: defaultPadding,
                   ),
-                  const Expanded(
-                      flex: 2,
-                      child:  StorageDetails())
+                  const Expanded(flex: 2, child: StorageDetails())
                 ],
               )
             ],
